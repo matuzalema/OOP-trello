98722797dd70790e6498fb7a78e35327b0c559e9
Jadwiga Bochinska <matuzalema@gmail.com>
2019-01-07 15:08:28 +0100

    fix sortable bug

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-// document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function() {
 function randomString() {
     var chars = '0123456789abcdefghiklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXTZ';
     var str = '';
@@ -119,4 +119,4 @@ todoColumn.addCard(card1);
 doingColumn.addCard(card2);
 doneColumn.addCard(card3);
 
-// });
+});
